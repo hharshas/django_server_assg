dc5ff4b7ab64e9457afff30b63bc9b211a30140e
hharshas <22je0388@iitism.ac.in>
2025-02-11 23:33:49 +0530

    pirates of carribean

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views 
+from .views import TimetableListView, TimetableDetailView, UserTimetableView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes),
+    path('timetable/', TimetableListView.as_view(), name='timetable-list'),
+    path('timetable/<int:pk>/', TimetableDetailView.as_view(), name='timetable-detail'),
+    path('timetable/<str:username>/', UserTimetableView.as_view(), name='user-timetable'),
+]
---
+++ api/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from api.models import User, Timetable
+
+from api.serializer import MyTokenObtainPairSerializer, RegisterSerializer, TimetableSerializer, UserSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class TimetableListView(generics.ListCreateAPIView):
+    serializer_class = TimetableSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Timetable.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TimetableDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TimetableSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Timetable.objects.filter(user=self.request.user)
+
+class UserTimetableView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        timetable = Timetable.objects.filter(user=user)
+        serializer = TimetableSerializer(timetable, many=True)
+        return Response(serializer.data)
+
+# Get All Routes
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/'
+    ]
+    return Response(routes)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def testEndPoint(request):
+    if request.method == 'GET':
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = "Hello buddy"
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
+
+    
